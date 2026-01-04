74a6b19db72f6d4b11c682be79a1cb357cab429a
NEXNC <git@nexnc.com>
2026-01-04 18:41:51 +0530

    fixed gtk and qt dark mode issues

--- home/modules/desktop/theme.nix
+++ home/modules/desktop/theme.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }:
-
 {
   # 1. Force GTK Theme (Controls Firefox, Thunar, etc.)
   gtk = {
@@ -16,13 +15,22 @@
         variant = "mocha";
       };
     };
-
+    
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
+    
+    # Add these lines to force dark mode preference
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
-
+  
   # 2. Force Qt to look like GTK (Fixes KDE Connect visual issues)
   qt = {
     enable = true;
