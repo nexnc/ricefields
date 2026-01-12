ea7883df6ee617aee83e3163ec481f39d4ffc60c
NEXNC <git@nexnc.com>
2026-01-12 19:21:13 +0530

    code refactor, added ./cpp and renamed python-uv to python

--- templates/flake.nix
+++ templates/flake.nix
@@ -1,23 +1,13 @@
 {
-  description = "Master Template Collection";
+  description = "Project templates";
 
   outputs = { self }: {
     templates = {
       # The default template (nix flake init -t univ)
-      blank = {
-        path = ./blank;
-        description = "Minimal flake with direnv auto-loading";
-      };
-
-      python-uv = {
-        path = ./python-uv;
-        description = "Python environment with uv and direnv";
-      };
-
-      rust = {
-        path = ./rust;
-        description = "Rust development environment (Cargo/Analyzer)";
-      };
+      blank = { path = ./blank; description = "Minimal flake with direnv auto-loading"; };
+      python = { path = ./python; description = "Python environment with uv and direnv"; };
+      rust = { path = ./rust; description = "Rust development environment (Cargo/Analyzer)" ;};
+      cpp = { path = ./cpp; description = "C++ Clang/CMake environment"; };
     };
 
     defaultTemplate = self.templates.blank;
