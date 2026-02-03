a851abe3bc3c891774d66662f7ac0c44dbc57eb1
NEXNC <git@nexnc.com>
2026-02-03 11:08:17 +0530

    fixed color and theming issues to match foot

--- home/modules/programs/tmux.nix
+++ home/modules/programs/tmux.nix
@@ -7,7 +7,7 @@
     # Optional: Set your preferred settings
     shortcut = "a";  # Changes prefix to Ctrl-a (default is b)
     baseIndex = 1;   # Start window numbering at 1
-    terminal = "screen-256color";
+    terminal = "tmux-256color";
     
     # Enable mouse support
     mouse = true;
@@ -15,7 +15,7 @@
     # Key bindings and other settings
     extraConfig = ''
       # Reload config
-      bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded!"
+      bind r source-file $HOME/.config/tmux/tmux.conf \; display "Reloaded!"
       
       # Split panes using | and -
       bind | split-window -h
@@ -30,12 +30,16 @@
       set -g @continuum-restore 'on'
       set -g @continuum-save-interval '15'
       
-      # Optional: Show continuum status in status bar
+      # Show continuum status in status bar
       set -g status-right 'Continuum: #{continuum_status}'
       
-      # Optional: Resurrect additional settings
+      # Resurrect additional settings
       set -g @resurrect-capture-pane-contents 'on'
       set -g @resurrect-strategy-vim 'session'
+      
+      # Color Fixes
+      set-option -ga terminal-overrides ",foot:Tc"
+      set -as terminal-features ",foot:RGB"
     '';
     
     # Plugins
@@ -44,6 +48,13 @@
       resurrect
       continuum
       yank
+      {
+    	plugin = catppuccin;
+    	extraConfig = ''
+      	  set -g @catppuccin_flavor 'mocha'
+          set -g @catppuccin_window_tabs_enabled on
+         '';
+      }
     ];
   };
 }
