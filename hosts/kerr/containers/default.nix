7f30bc0ac27494d14355cdda78b912d05b97c6c0
nexnc <git@nexnc.com>
2026-08-19 05:09:15 +0200

    feat: added lib under module scope, disable mirror pulling on boot to save boot time

--- hosts/kerr/containers/default.nix
+++ hosts/kerr/containers/default.nix
@@ -1,10 +1,21 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   virtualisation.oci-containers = {
     backend = "podman";
     containers = 
       (import ./portainer.nix) //
-      (import ./cloudflared.nix {inherit config pkgs;});
+      (import ./cloudflared.nix { inherit config pkgs; });
+  };
+
+  # Push container startup after graphical.target so the desktop loads immediately
+  systemd.services.podman-portainer = {
+    after = [ "graphical.target" ];
+    wantedBy = lib.mkForce [ "graphical.target" ];
+  };
+
+  systemd.services.podman-cloudflared = {
+    after = [ "graphical.target" ];
+    wantedBy = lib.mkForce [ "graphical.target" ];
   };
 }
